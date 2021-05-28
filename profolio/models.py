f0db55a37c130ec599f4a4726d16a55ccf4a45bd
Kevin <kevin891210@gmail.com>
2021-05-28 18:15:39 +0800

    V1

---
+++ profolio/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='profolio/images/')
+    url = models.URLField(blank=True)
